6a0e08c730d6aab082596cc5f7aea3fd9d78e8e3
Aleksandr Dmitriev <aleksandr_dmitriev_96@bk.ru>
2018-07-20 01:33:56 +0500

    Save changes

--- src/main/java/ru/projects/Shop/ApplicationConfig.java
+++ src/main/java/ru/projects/Shop/ApplicationConfig.java
@@ -7,12 +7,25 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import ru.projects.Shop.rest.AdressRestService;
+import ru.projects.Shop.rest.BonusCardRestService;
+import ru.projects.Shop.rest.BuyRestService;
+import ru.projects.Shop.rest.CityRestService;
+import ru.projects.Shop.rest.ClientRestService;
+import ru.projects.Shop.rest.CommentRestService;
+
 @ApplicationPath("api")
 public class ApplicationConfig extends Application {
 	private final Set<Class<?>> classes;
 	
 	public ApplicationConfig() {
 		HashSet<Class<?>> c=new HashSet();
+		c.add(AdressRestService.class);
+		c.add(BonusCardRestService.class);
+		c.add(BuyRestService.class);
+		c.add(CityRestService.class);
+		c.add(ClientRestService.class);
+		c.add(CommentRestService.class);
 		classes=Collections.unmodifiableSet(c);
 	}
 	
